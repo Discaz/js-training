2d7dfce9a89333bdfd1d8fd2f3c84ca3501312bd
Discaz <olivier.discazeaux@gmail.com>
2019-06-09 12:34:14 +0200

    update in Jaden Case

--- exercises/jaden-case.js
+++ exercises/jaden-case.js
@@ -9,20 +9,37 @@
  * Example : "How are you ?" -> "How Are You ?"
  *
  */
-const jadenCase = (string) => {
-  const words = string.split(' ');
+// function jadenCase (string){
+//   const words = string.split(' ');
   
-  for (let i = 0;i < words.lenght; i++) {
-  words[i] = words[i][0].toUpperCase() + words[i].slice(1);
-    }
-  return words.join(' ');
-  };
-
+//   for (let i = 0; i < words.length; i++) {
+//   words[i] = words[i][0].toUpperCase() + words[i].slice(1);
+//     }
+//   const jaden = words.join(' ')
+//   console.log(jaden)
+//   return jaden; 
+  
+//   };
+  
   
 
+// //* Begin of tests
+// const assert = require('assert')
+
+// assert.strictEqual(jadenCase('salut olivier comment va?'), 'Salut Olivier Comment Va?')
+// assert.fail('You must write your own tests')
+// // End of tests */
+
+function jadenCase(str) {
+  let words = str.split(' ');
+  for (let i = 0; i < words.length; i++) {
+      let firstLetterSplit = words[i].split('');
+      firstLetterSplit[0] = firstLetterSplit[0].toUpperCase();
+      words[i] = firstLetterSplit.join('');
+  }
+  words = words.join(' ');
+  return words;
+}
 //* Begin of tests
 const assert = require('assert')
-
-assert.strictEqual(jadenCase('salut olivier'), 'Salut Olivier')
-assert.fail('You must write your own tests')
-// End of tests */
+assert.strictEqual(jadenCase('how are you ?'), 'How Are You ?');
